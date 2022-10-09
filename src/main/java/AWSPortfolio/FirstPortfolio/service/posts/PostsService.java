cc035faf84cac70efaac00c57da5e785eafb2adc
chlcken2 <ckdgus988@naver.com>
2022-10-09 17:03:03 +0900

    post Controller 및 테스트 코드 작성

---
+++ src/main/java/AWSPortfolio/FirstPortfolio/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package AWSPortfolio.FirstPortfolio.service.posts;
+
+import AWSPortfolio.FirstPortfolio.domain.posts.PostsRepository;
+import AWSPortfolio.FirstPortfolio.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
