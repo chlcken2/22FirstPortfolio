5043afd4124f814a875121d578df1a575f51e82d
chlcken2 <ckdgus988@naver.com>
2022-10-10 00:10:38 +0900

    FEATURE 게시글 수정, 삭제 기능 추가

--- src/main/java/AWSPortfolio/FirstPortfolio/service/posts/PostsService.java
+++ src/main/java/AWSPortfolio/FirstPortfolio/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package AWSPortfolio.FirstPortfolio.service.posts;
 
 import AWSPortfolio.FirstPortfolio.domain.posts.Posts;
 import AWSPortfolio.FirstPortfolio.domain.posts.PostsRepository;
+import AWSPortfolio.FirstPortfolio.web.dto.PostsListResponseDto;
 import AWSPortfolio.FirstPortfolio.web.dto.PostsResponseDto;
 import AWSPortfolio.FirstPortfolio.web.dto.PostsSaveRequestDto;
 import AWSPortfolio.FirstPortfolio.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="  + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
+        postsRepository.delete(posts);
     }
 }
