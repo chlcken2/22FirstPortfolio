39f0fc2dddebfb77e5ab1c28e9e2876c6372efc3
chlcken2 <ckdgus988@naver.com>
2022-10-09 13:25:12 +0900

    Initial commit

---
+++ src/test/java/AWSPortfolio/FirstPortfolio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package AWSPortfolio.FirstPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
