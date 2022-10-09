e24a3df585921f5632e4065ec15abf347c1a0c9e
chlcken2 <ckdgus988@naver.com>
2022-10-09 15:53:01 +0900

    PostRepositoryTest# 게시글저장_불러오기

---
+++ src/main/java/AWSPortfolio/FirstPortfolio/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package AWSPortfolio.FirstPortfolio.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
