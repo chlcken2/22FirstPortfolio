423d7883b53ebd47ef267d7908bec58e57801c4b
chlcken2 <ckdgus988@naver.com>
2022-10-21 21:30:47 +0900

    Refactor 오타 수정

--- src/main/java/AWSPortfolio/FirstPortfolio/config/auth/dto/OAuthAttributes.java
+++ src/main/java/AWSPortfolio/FirstPortfolio/config/auth/dto/OAuthAttributes.java
@@ -49,9 +49,10 @@ public class OAuthAttributes {
 
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
         return OAuthAttributes.builder()
                 .name((String) response.get("name"))
-                .email((String) response.get("emial"))
+                .email((String) response.get("email"))
                 .picture((String) response.get("profile_image"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
