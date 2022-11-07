a0f43546c9a9d55932927dd34e205ec18c5c9004
chlcken2 <ckdgus988@naver.com>
2022-11-07 22:13:33 +0900

    FIX  서비스 네임 수정 및 AWS EC2, RDS, 도메인 생성 = hellomyteam

---
+++ src/main/java/HelloMyTeam/Hellomyteam/web/PostsApiController.java
@@ -0,0 +1,36 @@
+package HelloMyTeam.Hellomyteam.web;
+
+import HelloMyTeam.Hellomyteam.service.posts.PostsService;
+import HelloMyTeam.Hellomyteam.web.dto.PostsResponseDto;
+import HelloMyTeam.Hellomyteam.web.dto.PostsSaveRequestDto;
+import HelloMyTeam.Hellomyteam.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+}
