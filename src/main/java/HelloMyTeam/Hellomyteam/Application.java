a88607404a5501c4db88625d8102d8c4f99c7d8b
chlcken2 <ckdgus988@naver.com>
2022-11-13 18:46:58 +0900

    FEATURE appspec.yml 최상단 위치, 경로 수정

--- src/main/java/HelloMyTeam/Hellomyteam/Application.java
+++ src/main/java/HelloMyTeam/Hellomyteam/Application.java
@@ -2,6 +2,7 @@ package HelloMyTeam.Hellomyteam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -9,7 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
+
 	}
 
 }
