f7ed203b4b1f68fbf6286416e85c85095e1be324
chlcken2 <ckdgus988@naver.com>
2022-11-15 21:34:29 +0900

    FIX pid path 잘 몰라서 롤백 추후 추가 예정

--- src/main/java/HelloMyTeam/Hellomyteam/Application.java
+++ src/main/java/HelloMyTeam/Hellomyteam/Application.java
@@ -8,14 +8,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-//	public static void main(String[] args) {
-//		SpringApplication application = new SpringApplication(Application.class);
-//		application.addListeners(new ApplicationPidFileWriter());
-//		application.run(args);
-//
-//	}
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
+
 	}
+
 
 }
