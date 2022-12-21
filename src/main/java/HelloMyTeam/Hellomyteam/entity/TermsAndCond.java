dac6730cc67dd6dc7f44ae34e7cffb796c9d3a6f
chl.cken <ckdgus988@naver.com>
2022-12-21 11:15:18 +0900

    FEATURE: 이용약관 관련 내용 분리 및 Enum처리

---
+++ src/main/java/HelloMyTeam/Hellomyteam/entity/TermsAndCond.java
@@ -0,0 +1,28 @@
+package HelloMyTeam.Hellomyteam.entity;
+
+import HelloMyTeam.Hellomyteam.entity.status.TermsAndCondStatus;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class TermsAndCond {
+    @Id
+    @GeneratedValue
+    @Column(name = "termsAndCond_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private TermsAndCondStatus termsOfServiceYn;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private TermsAndCondStatus privacyYn;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
