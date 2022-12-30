de0b6ec56b0f9bed3d8b672112686ca9fe3ccc31
hiSuji <sujikim.dev@gmail.com>
2022-12-30 19:13:06 +0900

    머지 충돌 파일 복구

--- src/main/java/HelloMyTeam/Hellomyteam/dto/MemberDTO.java
+++ src/main/java/HelloMyTeam/Hellomyteam/dto/MemberDTO.java
@@ -1,24 +1,18 @@
 package HelloMyTeam.Hellomyteam.dto;
 
-import HelloMyTeam.Hellomyteam.entity.Team;
-import HelloMyTeam.Hellomyteam.entity.status.MemberStatus;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDTO {
     private Long id;
-    private String mobile;
-    private String name;
     private String email;
-    private LocalDate birthday;
-    private MemberStatus memberStatus;
-    private Team team;
+    private String name;
+    private String birthday;
+    private String memberStatus;
 }
