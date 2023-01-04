11a2040261311a41cc650a4cd1dfcf0349c8a426
chl.cken <ckdgus988@naver.com>
2023-01-04 19:31:20 +0900

    FIX deploy.sh cd 내용 적용

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,60 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-REPOSITORY=/usr/local/myapp
-cd $REPOSITORY
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=hellomyteam #해당 위치에 properties에 작성한 프로젝트명과 동일하게 작성합니다.
 
-APP_NAME=myapp
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
 
-echo "> Kill Previous Process" >> /usr/local/myapp/deploy.log
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]
-then
-  echo "> No Process to Kill" >> /usr/local/myapp/deploy.log
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
-  echo "> Kill $CURRENT_PID" >> /usr/local/myapp/deploy.log
-  kill -15 "$CURRENT_PID" >> /usr/local/myapp/deploy.log 2>&1
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH Deploy"
-nohup java -jar $JAR_PATH >> /usr/local/myapp/nohup.log 2>&1 &
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
+echo "> JAR_NAME: $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
 
-#
-##!/bin/bash
-#
-#REPOSITORY=/home/ec2-user/app/step2
-#PROJECT_NAME=hellomyteam #해당 위치에 properties에 작성한 프로젝트명과 동일하게 작성합니다.
-#
-#echo "> Build 파일 복사"
-#cp $REPOSITORY/zip/*.jar $REPOSITORY/
-#
-#echo "> 현재 구동 중인 애플리케이션 pid 확인"
-#CURRENT_PID=$(pgrep -f $PROJECT_NAME)
-#
-#echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-#
-#if [ -z "$CURRENT_PID" ]; then
-#  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
-#else
-#  echo "> kill -15 $CURRENT_PID"
-#  kill -15 $CURRENT_PID
-#  sleep 5
-#fi
-#
-#echo "> 새 애플리케이션 배포"
-#JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-#
-#echo "> JAR_NAME: $JAR_NAME"
-#echo "> $JAR_NAME 에 실행권한 추가"
-#chmod +x $JAR_NAME
-#
-#echo "> $JAR_NAME 실행"
-#nohup java -jar \
-#  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-#  -Dspring.profile.active=real \
-#  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_NAME 실행"
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-dev.yml \
+  -Dspring.profile.active=dev \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
