57e9ae46b0b3bfefcf59401f0179c04523777029
chl.cken <ckdgus988@naver.com>
2023-01-15 20:42:53 +0900

    FIX 프론트 api호출시 오류 발생으로 인한 /api 추가

--- src/main/java/HelloMyTeam/Hellomyteam/controller/AuthController.java
+++ src/main/java/HelloMyTeam/Hellomyteam/controller/AuthController.java
@@ -24,7 +24,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
     public static final String AUTHORIZATION_HEADER = "Authorization";
