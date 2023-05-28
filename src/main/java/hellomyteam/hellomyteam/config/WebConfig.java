9ecd29dad7dc5a40a3275508a4f53aa72a32e8fc
chl.cken <ckdgus988@naver.com>
2023-05-28 13:22:36 +0900

    HOSTFIX: CORS에 프론트 local URL 추가

--- src/main/java/hellomyteam/hellomyteam/config/WebConfig.java
+++ src/main/java/hellomyteam/hellomyteam/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins("https://hellomyteam.com",
                         "https://www.hellomyteam.com",
+                        "http://localhost:3000/",
                         "https://hellomyteam-front-o8od.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
