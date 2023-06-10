b04d3b59999e380bd083c769cb2f4f129bfdda1b
chl.cken <ckdgus988@naver.com>
2023-06-11 01:58:00 +0900

    Test: webconfig.java 삭제

--- src/main/java/hellomyteam/hellomyteam/config/WebConfig.java
+++ src/main/java/hellomyteam/hellomyteam/config/WebConfig.java
@@ -1,18 +1,18 @@
-package hellomyteam.hellomyteam.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
-    }
-}
-
-
+//package hellomyteam.hellomyteam.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*");
+//    }
+//}
+//
+//
